79f78fd1af3e160c1844d239a70ccf09934acba3
Босенко Артем <artem.bosenko@nure.ua>
2021-04-16 19:40:30 +0300

    add configure

---
+++ unit_7_collection_1/src/main/java/Test.java
@@ -0,0 +1,10 @@
+import ua.com.hometaskcollection.SortedList;
+
+public class Test {
+    public void test(){
+        SortedList<Integer> list = new SortedList<>(5);
+
+        System.out.println(list.size());
+        System.out.println(list.isEmpty());
+    }
+}
